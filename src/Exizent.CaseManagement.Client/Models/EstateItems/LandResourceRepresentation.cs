144924164de4f39750586dcddab81d1a600eaccc
Aaron Ashworth <aaron.ashworth@exizent.com>
2022-05-23 08:46:10 +0100

    removed IsHeritable from Land REsourceRepresenatation as  it is always true

--- src/Exizent.CaseManagement.Client/Models/EstateItems/LandResourceRepresentation.cs
+++ src/Exizent.CaseManagement.Client/Models/EstateItems/LandResourceRepresentation.cs
@@ -36,6 +36,5 @@ public class LandResourceRepresentation : EstateItemResourceRepresentation
     public bool IsSubjectToSpecialFactors { get; init; }
     public string? SpecialFactorsDescription { get; init; }
     public bool IsCharityDonation { get; init; }
-    public bool IsHeritable { get; init; }
     public EstateItemRealisationResourceRepresentation Realisation { get; init; } = null!;
 }
