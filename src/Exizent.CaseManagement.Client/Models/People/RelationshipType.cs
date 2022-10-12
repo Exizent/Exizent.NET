d1391e0b3f7ff8f92cca8ff4704b2d381f630978
katiemorton <katie.morton@exizent.com>
2022-10-12 09:02:32 +0100

    added extra relationship statuses

--- src/Exizent.CaseManagement.Client/Models/People/RelationshipType.cs
+++ src/Exizent.CaseManagement.Client/Models/People/RelationshipType.cs
@@ -1,7 +1,12 @@
-﻿namespace Exizent.CaseManagement.Client.Models.People;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Exizent.CaseManagement.Client.Models.People;
 
 public enum RelationshipType
 {
+
     Husband = 1,
     Wife = 2,
     BiologicalChild = 3,
@@ -17,5 +22,14 @@ public enum RelationshipType
     GreatGrandchild = 13,
     Cousin = 14,
     Friend = 15,
-    Other = 16
+    Other = 16,
+    Solicitor = 17,
+    MotherInLaw = 18,
+    FatherInLaw = 19,
+    SonInLaw = 20,
+    DaughterInLaw = 21,
+    BrotherInLaw = 22,
+    SisterInLaw = 23,
+    Partner = 24
+
 }
