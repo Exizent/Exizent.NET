b75629fde5d85bb4d05915b94d8f61e719facc87
Aaron Ashworth <aaron.ashworth@exizent.com>
2023-03-17 09:53:08 +0000

    Checnged Exemption value to a decimal nullable

--- src/Exizent.CaseManagement.Client/Models/EstateItems/FinancialGiftResourceRepresentation.cs
+++ src/Exizent.CaseManagement.Client/Models/EstateItems/FinancialGiftResourceRepresentation.cs
@@ -9,7 +9,7 @@ public class FinancialGiftResourceRepresentation : EstateItemResourceRepresentat
     public string? RecipientSurname { get; init; }
     public string? Description { get; init; }
     public decimal? GrossValue { get; init; }
-    public decimal ExemptionValue { get; init; }
+    public decimal? ExemptionValue { get; init; }
     public DateTime? DateOfGift { get; init; }
     public FinancialGiftExemptionCategory? ExemptionCategory { get; init; }
 }
