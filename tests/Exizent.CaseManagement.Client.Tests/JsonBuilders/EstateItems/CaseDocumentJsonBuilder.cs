5b94ddf8480d2e350cf6170fab32240c335484bf
Aaron Ashworth <aaron.ashworth@exizent.com>
2023-10-03 12:13:54 +0100

    Added metadata to document representations

--- tests/Exizent.CaseManagement.Client.Tests/JsonBuilders/EstateItems/CaseDocumentJsonBuilder.cs
+++ tests/Exizent.CaseManagement.Client.Tests/JsonBuilders/EstateItems/CaseDocumentJsonBuilder.cs
@@ -4,18 +4,29 @@ using Exizent.CaseManagement.Client.Models.EstateItems;
 
 namespace Exizent.CaseManagement.Client.Tests.JsonBuilders.EstateItems;
 
-public class CaseDocumentJsonBuilder 
+public class CaseDocumentJsonBuilder
 {
     public static JsonObject Build(CaseDocumentResourceRepresentation resourceRepresentation) => new()
     {
-        {"id", resourceRepresentation.Id},
-        {"createdAt", resourceRepresentation.CreatedAt},
-        {"key", resourceRepresentation.Key},
-        {"fileName", resourceRepresentation.FileName},
-        {"fileSize", resourceRepresentation.FileSize},
-        {"updatedAt", resourceRepresentation.UpdatedAt},
-        {"documentType", resourceRepresentation.DocumentType.ToString()},
-        {"isInfected", resourceRepresentation.IsInfected},
+        { "id", resourceRepresentation.Id },
+        { "createdAt", resourceRepresentation.CreatedAt },
+        { "key", resourceRepresentation.Key },
+        { "fileName", resourceRepresentation.FileName },
+        { "fileSize", resourceRepresentation.FileSize },
+        { "updatedAt", resourceRepresentation.UpdatedAt },
+        { "documentType", resourceRepresentation.DocumentType.ToString() },
+        { "isInfected", resourceRepresentation.IsInfected },
+        { "metadata", Build(resourceRepresentation.Metadata) }
     };
 
+    public static JsonObject Build(Dictionary<string, string> metadata)
+    {
+        var obj = new JsonObject();
+        foreach (var item in metadata)
+        {
+            obj.Add(item.Key, item.Value);
+        }
+
+        return obj;
+    }
 }
