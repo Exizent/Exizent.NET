b189c8ab4ce8b9aeb86b669a6173e1336bdcaf79
Ben Davidson <ben.davidson@exizent.com>
2024-03-12 10:40:42 +0000

    Adding final tests for updating collaborators

--- tests/Exizent.CaseManagement.Client.IntegrationTests/UpdatingCollaborators.cs
+++ tests/Exizent.CaseManagement.Client.IntegrationTests/UpdatingCollaborators.cs
@@ -117,4 +117,110 @@ public class UpdatingCollaborators
         var action = async () => await client.UpdateCaseOwner(caseId, ownerId);
         await action.Should().ThrowAsync<HttpRequestException>().WithMessage(expectedMessage);
     }
+
+    [Fact]
+    public async Task ShouldUpdateCaseCollaborators()
+    {
+        var clientId = Guid.NewGuid().ToString();
+        var clientSecret = Guid.NewGuid().ToString();
+
+        var caseId = Guid.NewGuid();
+        var collaborators = new List<int> { 1, 2, 3 };
+
+        using var authApiServer = WireMockServer.Start();
+        authApiServer
+            .Given(Request.Create().WithPath("/oauth/token")
+                .WithBody(new JsonPartialMatcher(
+                    $@"{{ ""grant_type"": ""client_credentials"", ""client_id"": ""{clientId}"", ""client_secret"": ""{clientSecret}"", ""scope"": ""{ExizentScopes.All}"", ""audience"": ""https://resources.exizent.com"" }}"))
+                .UsingPost())
+            .RespondWith(Response.Create()
+                .WithBody("{ \"access_token\": \"123456\", \"expires_in\": 3600, \"token_type\": \"Bearer\" }")
+            );
+
+        using var casesApiServer = WireMockServer.Start();
+        casesApiServer.Given(
+                Request.Create().WithPath($"/cases/{caseId}/collaborators").UsingPut()
+                    .WithHeader("User-Agent", "My browser")
+            )
+            .RespondWith(Response.Create()
+                .WithHeader("Authorization", "Bearer 123456")
+                .WithStatusCode(HttpStatusCode.OK)
+            );
+
+        var baseUri = casesApiServer.Url;
+        var baseAuthorizationUri = authApiServer.Url;
+
+        var serviceContainer = new ServiceCollection();
+        serviceContainer.AddExizentCaseManagementClient(
+            clientId,
+            clientSecret,
+            settings =>
+            {
+                settings.BaseUri = new Uri(baseUri);
+                settings.BaseAuthorizationUri = new Uri(baseAuthorizationUri);
+                settings.Scope = ExizentScopes.All;
+                settings.UserAgent = "My browser";
+            }
+        );
+
+        await using var serviceProvider = serviceContainer.BuildServiceProvider();
+        using var scope = serviceProvider.CreateScope();
+        var client = scope.ServiceProvider.GetRequiredService<ICaseManagementApiClient>();
+
+        await client.UpdateCaseCollaborators(caseId, collaborators);
+    }
+
+    [Theory]
+    [InlineData(HttpStatusCode.NotFound, "Response status code does not indicate success: 404 (Not Found).")]
+    [InlineData(HttpStatusCode.BadRequest, "Response status code does not indicate success: 400 (Bad Request).")]
+    public async Task ShouldThrowExceptionWhenUpdateCaseCollaboratorsFails(HttpStatusCode statusCode, string expectedMessage)
+    {
+        var clientId = Guid.NewGuid().ToString();
+        var clientSecret = Guid.NewGuid().ToString();
+
+        var caseId = Guid.NewGuid();
+
+        using var authApiServer = WireMockServer.Start();
+        authApiServer
+            .Given(Request.Create().WithPath("/oauth/token")
+                .WithBody(new JsonPartialMatcher(
+                    $@"{{ ""grant_type"": ""client_credentials"", ""client_id"": ""{clientId}"", ""client_secret"": ""{clientSecret}"", ""scope"": ""{ExizentScopes.All}"", ""audience"": ""https://resources.exizent.com"" }}"))
+                .UsingPost())
+            .RespondWith(Response.Create()
+                .WithBody("{ \"access_token\": \"123456\", \"expires_in\": 3600, \"token_type\": \"Bearer\" }")
+            );
+
+        using var casesApiServer = WireMockServer.Start();
+        casesApiServer.Given(
+                Request.Create().WithPath($"/cases/{caseId}/collaborators").UsingPut()
+                    .WithHeader("User-Agent", "My browser")
+            )
+            .RespondWith(Response.Create()
+                .WithHeader("Authorization", "Bearer 123456")
+                .WithStatusCode(statusCode)
+            );
+
+        var baseUri = casesApiServer.Url;
+        var baseAuthorizationUri = authApiServer.Url;
+
+        var serviceContainer = new ServiceCollection();
+        serviceContainer.AddExizentCaseManagementClient(
+            clientId,
+            clientSecret,
+            settings =>
+            {
+                settings.BaseUri = new Uri(baseUri);
+                settings.BaseAuthorizationUri = new Uri(baseAuthorizationUri);
+                settings.Scope = ExizentScopes.All;
+                settings.UserAgent = "My browser";
+            }
+        );
+
+        await using var serviceProvider = serviceContainer.BuildServiceProvider();
+        using var scope = serviceProvider.CreateScope();
+        var client = scope.ServiceProvider.GetRequiredService<ICaseManagementApiClient>();
+
+        var action = async () => await client.UpdateCaseCollaborators(caseId, new List<int> { 1, 2, 3 });
+        await action.Should().ThrowAsync<HttpRequestException>().WithMessage(expectedMessage);
+    }
 }
