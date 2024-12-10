7dd11c091985a7ce1f2234185c02454d0ed15ea2
Aaron Ashworth <aaron.ashworth@exizent.com>
2024-12-10 15:05:35 +0000

    be more explicit with the values per SourceOfFunds enum member member

--- src/Exizent.CaseManagement.Client/Models/Expenses/SourceOfFunds.cs
+++ src/Exizent.CaseManagement.Client/Models/Expenses/SourceOfFunds.cs
@@ -3,16 +3,16 @@ namespace Exizent.CaseManagement.Client.Models.Expenses;
 public enum SourceOfFunds
 {
     DeceasedBankAccount = 1,
-    CashSavingsAccount,
-    CashIsa,
-    ClientCashAccount,
-    ProceedsOfSale,
-    PrepaidFuneralPlan,
-    PremiumBond,
-    NationalSavingsAndInvestmentsProduct,
+    CashSavingsAccount = 2,
+    CashIsa = 3,
+    ClientCashAccount = 4,
+    ProceedsOfSale = 5,
+    PrepaidFuneralPlan = 6,
+    PremiumBond = 7,
+    NationalSavingsAndInvestmentsProduct = 8,
     NationalSavingsAndInvestmentsIncomeBond = 9,
-    Pension,
-    LifePolicy,
 #pragma warning disable CA1069 // Enums values should not be duplicated
-    IncomeBond = 9
+    IncomeBond = 9,
+    Pension = 10,
+    LifePolicy = 11
 }
