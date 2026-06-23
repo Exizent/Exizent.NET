631f731c684b2065a254339f4faa8c0860ecc6ea
Kevin Smith <kev_bite@msn.com>
2026-06-23 13:59:06 +0100

    [FD-1349]: Add serialisation tests for ShareSettlement and PaySurplus on expense settlement

--- tests/Exizent.CaseManagement.Client.Tests/GettingACaseTests/GettingACaseWithExpenses.cs
+++ tests/Exizent.CaseManagement.Client.Tests/GettingACaseTests/GettingACaseWithExpenses.cs
@@ -52,4 +52,56 @@ public sealed class GettingACaseWithExpenses : IClassFixture<Harness>
             .BeEquivalentTo(expectedExpense);
     }
 
+    [Fact]
+    public async Task ShouldDeserialiseShareSettlementWithPaySurplusOnExpenseSettlement()
+    {
+        var bankAccountId = Guid.NewGuid();
+        var settlement = new ExpenseSettlementResourceRepresentation
+        {
+            Id = Guid.NewGuid(),
+            Value = 100m,
+            SourceOfFunds = SourceOfFunds.ProceedsOfSale,
+            At = DateTime.UtcNow,
+            ShareSettlement = new ShareSettlementDetailsResourceRepresentation
+            {
+                Quantity = 10,
+                Price = 1000m,
+                PaySurplus = new PaySurplusDestinationResourceRepresentation
+                {
+                    Type = PaySurplusToType.BankAccount,
+                    CaseItemId = bankAccountId
+                }
+            }
+        };
+        var expense = new ExpenseResourceRepresentation
+        {
+            Id = Guid.NewGuid(),
+            Description = "Share Sale",
+            CaseItemId = Guid.NewGuid(),
+            Value = 60m,
+            From = DateTime.UtcNow,
+            To = DateTime.UtcNow,
+            Supplier = "Broker",
+            Purpose = ExpensePurposeType.AssetSaleOrLiabilityRepayment,
+            Settlements = new List<ExpenseSettlementResourceRepresentation> { settlement }
+        };
+        var caseResourceRepresentation = new CaseResourceRepresentationBuilder()
+            .With(expense)
+            .Build();
+
+        _harness.ClientHandler.AddGetCaseResponse(caseResourceRepresentation.Id,
+            CaseJsonBuilder.Build(caseResourceRepresentation).ToJsonString());
+
+        var caseDetails = await _harness.Client.GetCase(caseResourceRepresentation.Id);
+
+        using var _ = new AssertionScope();
+        var returnedSettlement = caseDetails!.Expenses.Single().Settlements.Single();
+        returnedSettlement.ShareSettlement.Should().NotBeNull();
+        returnedSettlement.ShareSettlement!.Quantity.Should().Be(10);
+        returnedSettlement.ShareSettlement.Price.Should().Be(1000m);
+        returnedSettlement.ShareSettlement.Value.Should().Be(100m);  // 10 * 1000 / 100
+        returnedSettlement.ShareSettlement.PaySurplus!.Type.Should().Be(PaySurplusToType.BankAccount);
+        returnedSettlement.ShareSettlement.PaySurplus.CaseItemId.Should().Be(bankAccountId);
+    }
+
 }
--- tests/Exizent.CaseManagement.Client.Tests/JsonBuilders/Expenses/ExpenseSettlementJsonBuilder.cs
+++ tests/Exizent.CaseManagement.Client.Tests/JsonBuilders/Expenses/ExpenseSettlementJsonBuilder.cs
@@ -16,11 +16,36 @@ public class ExpenseSettlementJsonBuilder
             { "estateItemId", resourceRepresentation.EstateItemId },
             { "personId", resourceRepresentation.PersonId },
             { "generateIht423", resourceRepresentation.GenerateIht423 },
+            { "shareSettlement", BuildShareSettlement(resourceRepresentation.ShareSettlement) },
         };
     }
 
-    public static JsonArray Build (List<ExpenseSettlementResourceRepresentation> settlements)
+    public static JsonArray Build(List<ExpenseSettlementResourceRepresentation> settlements)
     {
         return new JsonArray(settlements.Select(Build).ToArray<JsonNode>());
+    }
+
+    private static JsonNode? BuildShareSettlement(ShareSettlementDetailsResourceRepresentation? shareSettlement)
+    {
+        if (shareSettlement is null) return null;
+
+        return new JsonObject
+        {
+            { "quantity", shareSettlement.Quantity },
+            { "price", shareSettlement.Price },
+            { "paySurplus", BuildPaySurplus(shareSettlement.PaySurplus) },
+        };
+    }
+
+    private static JsonNode? BuildPaySurplus(PaySurplusDestinationResourceRepresentation? paySurplus)
+    {
+        if (paySurplus is null) return null;
+
+        return new JsonObject
+        {
+            { "type", paySurplus.Type?.ToString("G") },
+            { "caseItemId", paySurplus.CaseItemId },
+            { "other", paySurplus.Other },
+        };
     }
 }
